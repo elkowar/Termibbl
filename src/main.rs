a5911508debbb36bfc280597314f042e81a92dac
Fola <30976147+fl-w@users.noreply.github.com>
2021-05-04 12:45:44 +0100

    remove client-code from main, initialise server and game room components

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,114 @@
-pub mod client;
-pub mod data;
-pub mod message;
-pub mod server;
+#![allow(dead_code, unused_variables)]
+mod client;
+mod data;
+mod encoding;
+mod events;
+mod message;
+mod server;
+mod utils;
+
+use client::App;
+use data::GameOpts;
+use events::EventSender;
+use server::GameServer;
+use utils::dispatch_abortable_task;
 
 use argh::FromArgs;
-use log::info;
 
-use std::io::{stdout, Write};
+use std::{error::Error, net::SocketAddr};
 
-use crossterm::{
-    event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, MouseEvent},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    Result,
-};
-
-use tui::{backend::CrosstermBackend, Terminal};
-
-use client::app::ServerSession;
-use data::Username;
-pub use serde::{Deserialize, Serialize};
-
-#[derive(FromArgs)]
 /// A Skribbl.io-alike for the terminal
+#[derive(FromArgs)]
 struct Opt {
     #[argh(subcommand)]
-    cmd: SubOpt,
+    cmd: Option<SubOpt>,
+
+    #[argh(switch, description = "write out debug logs.")]
+    log_debug: bool,
 }
 
 #[derive(FromArgs)]
 #[argh(subcommand)]
 enum SubOpt {
-    Server(server::CliOpts),
     Client(client::CliOpts),
+    Server(server::CliOpts),
 }
 
-fn display_public_ip(port: u32) {
-    tokio::spawn(async move {
-        if let Ok(res) = reqwest::get("http://ifconfig.me").await {
-            if let Ok(ip) = res.text().await {
-                println!("Your public IP is {}:{}", ip, port);
-                info!("You can find out your private IP by running \"ip addr\" in the terminal");
-            }
-        }
-    });
+async fn process_ctrl_c(tx: EventSender<server::Message>) {
+    let _ = tokio::signal::ctrl_c().await;
+
+    println!("✨ Ctrl-C received. Stopping..");
+    tx.send_with_urgency(server::Message::CtrlC)
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    pretty_env_logger::init();
-
+async fn main() -> Result<(), Box<dyn Error>> {
     let cli: Opt = argh::from_env();
-    match cli.cmd {
+
+    // default command to 'client' if non is passed
+    let cmd = cli
+        .cmd
+        .unwrap_or_else(|| SubOpt::Client(client::CliOpts::default()));
+
+    match cmd {
         SubOpt::Client(opt) => {
-            let addr = opt.addr;
-            let addr = if addr.starts_with("ws://") || addr.starts_with("wss://") {
-                addr
-            } else {
-                format!("ws://{}", addr)
-            };
-            run_client(&addr, opt.username.into()).await.unwrap();
+            let mut app = App::default();
+            let localhost = opt.port.map(|port| format!("127.0.0.1:{}", port));
+
+            if let Some(addr) = opt.host.or(localhost) {
+                app.set_host_input(addr.clone());
+
+                if let Ok(addr) = addr.parse::<SocketAddr>() {
+                    app.connect_to_server(addr);
+                }
+            }
+
+            if let Some(name) = opt.username {
+                app.set_name_input(name)
+            }
+
+            app.start().await?;
         }
 
-        SubOpt::Server(opt) => {
-            let port = opt.port;
+        SubOpt::Server(opts) => {
+            let log_level_filter = if cli.log_debug {
+                log::LevelFilter::Debug
+            } else {
+                log::LevelFilter::Info
+            };
+
+            pretty_env_logger::formatted_builder()
+                .filter(Some("termibbl"), log_level_filter)
+                .init();
+
+            let port = opts.port;
 
             // display public ip
-            if opt.display_public_ip {
-                display_public_ip(port);
+            if opts.display_public_ip {
+                tokio::spawn(async move {
+                    if let Ok(res) = reqwest::get("http://ifconfig.me").await {
+                        if let Ok(ip) = res.text().await {
+                            println!("Your public IP is {}:{}", ip, port);
+                            println!("You can find out your private IP by running \"ip addr\" in the terminal");
+                        }
+                    }
+                });
             }
 
-            // let default_game_opts: GameOpts = opt.into();
-            // let server_listener = server::listen(port);
+            let mut default_game_opts: GameOpts = opts.into();
+            let default_words = default_game_opts.custom_words.drain(..).collect();
+            let server = GameServer::new(default_game_opts, default_words);
+            let addr = format!("127.0.0.1:{}", port);
 
-            server::server::run_server(opt).await.unwrap();
+            // listen for ctrl_c
+            let ctrlc_abort_handle =
+                dispatch_abortable_task(process_ctrl_c(server.sender().clone()));
+
+            println!("🚀 Running Termibbl server on port {}...", port);
+            server.listen_on(&addr).await?;
+            ctrlc_abort_handle.abort();
         }
-    }
-    Ok(())
-}
+    };
 
-pub enum ClientEvent {
-    MouseInput(MouseEvent),
-    KeyInput(KeyEvent),
-    ServerMessage(message::ToClientMsg),
-}
-
-async fn run_client(addr: &str, username: Username) -> client::error::Result<()> {
-    let (mut client_evt_send, client_evt_recv) = tokio::sync::mpsc::channel::<ClientEvent>(1);
-
-    let mut app =
-        ServerSession::establish_connection(addr, username, client_evt_send.clone()).await?;
-
-    enable_raw_mode()?;
-    execute!(stdout(), EnterAlternateScreen)?;
-    execute!(stdout(), EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout());
-    let mut terminal = Terminal::new(backend)?;
-    tokio::spawn(async move {
-        app.run(&mut terminal, client_evt_recv).await.unwrap();
-    });
-    loop {
-        match read()? {
-            Event::Key(evt) => match evt {
-                KeyEvent {
-                    code: KeyCode::Esc,
-                    modifiers: _,
-                } => break,
-                _ => {
-                    let _ = client_evt_send.send(ClientEvent::KeyInput(evt)).await;
-                }
-            },
-            Event::Mouse(evt) => {
-                let _ = client_evt_send.send(ClientEvent::MouseInput(evt)).await;
-            }
-            _ => {}
-        }
-    }
-
-    execute!(stdout(), DisableMouseCapture)?;
-    execute!(stdout(), LeaveAlternateScreen)?;
-    disable_raw_mode()?;
     Ok(())
 }
