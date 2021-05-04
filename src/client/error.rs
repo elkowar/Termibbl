83dea3d6d7db3085f29196f14d6ded4631605d00
Fola <30976147+fl-w@users.noreply.github.com>
2021-05-04 12:58:46 +0100

    Update src/client/error.rs

--- src/client/error.rs
+++ src/client/error.rs
@@ -1,32 +1,25 @@
+use thiserror::Error;
+
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
+    #[error("send error: `{0}`")]
     SendError(String),
-    CrosstermError(crossterm::ErrorKind),
-    IOError(std::io::Error),
-    WebSocketError(tungstenite::error::Error),
+    #[error("task complete error")]
+    TaskError(#[from] tokio::task::JoinError),
+    #[error("crossterm error")]
+    TermError(#[from] crossterm::ErrorKind),
+    #[error("IO error")]
+    IOError(#[from] std::io::Error),
 }
 
 impl<T> From<tokio::sync::mpsc::error::SendError<T>> for Error {
-    fn from(e: tokio::sync::mpsc::error::SendError<T>) -> Self {
+    fn from(e: tokio::sync::mpsc::error::SendError<T>) -> Self { Error::SendError(e.to_string()) }
+}
+
+impl<T> From<tokio::sync::mpsc::error::TrySendError<T>> for Error {
+    fn from(e: tokio::sync::mpsc::error::TrySendError<T>) -> Self {
         Error::SendError(e.to_string())
-    }
-}
-impl From<crossterm::ErrorKind> for Error {
-    fn from(e: crossterm::ErrorKind) -> Self {
-        Error::CrosstermError(e)
-    }
-}
-
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Self {
-        Error::IOError(e)
-    }
-}
-
-impl From<tungstenite::error::Error> for Error {
-    fn from(e: tungstenite::error::Error) -> Self {
-        Error::WebSocketError(e)
     }
 }
